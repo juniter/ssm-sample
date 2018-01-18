b6d40739015bae50ca1a2332d99e7e41d0f85295
Juniter <juniterr@outlook.com>
2018-01-18 11:27:39 +0800

    update project

--- src/main/java/com/sy/controller/SampleController.java
+++ src/main/java/com/sy/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +20,31 @@ public class SampleController {
 		logger.info("请求信息:{}",sample.toString());
 	}
 
-	@RequestMapping(value="/rect",method = RequestMethod.GET,produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value="/rect")
 	public ModelAndView redirectPage(ModelAndView mav){
-		logger.info("重定向至新页面");
+		logger.info("重定向至新页面0");
 		mav.setViewName("sview");
+		mav.getModel().put("email","juniter@outlook.com");
+		return mav;
+	}
+	@GetMapping(value="/rect1")
+	public ModelAndView redirectPage1(ModelAndView mav){
+		logger.info("重定向至新页面1");
+		mav.setViewName("/sview");
+		mav.getModel().put("email","juniter@outlook.com");
+		return mav;
+	}
+	@GetMapping(value="/rect2")
+	public ModelAndView redirectPage2(ModelAndView mav){
+		logger.info("重定向至新页面2");
+		mav.setViewName("templates/sview");
+		mav.getModel().put("email","juniter@outlook.com");
+		return mav;
+	}
+	@GetMapping(value="/rect3")
+	public ModelAndView redirectPage3(ModelAndView mav){
+		logger.info("重定向至新页面3");
+		mav.setViewName("/templates/sview");
 		mav.getModel().put("email","juniter@outlook.com");
 		return mav;
 	}
--- src/main/java/com/sy/controller/conf/ErrorController.java
+++ src/main/java/com/sy/controller/conf/ErrorController.java
@@ -3,7 +3,6 @@ package com.sy.controller.conf;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
